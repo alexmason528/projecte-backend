30302a5276a418f0735c30ff861032dff13186b5
Alex Mason <alexmason528@hotmail.com>
2018-12-21 11:16:46 -0500

    Added trailing slashes to the url

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from .views import LogInView, RegisterView, VerifyEmailView, PasswordResetView, ProfileView
 
 urlpatterns = [
-    url(r'^login$', LogInView.as_view()),
-    url(r'^register$', RegisterView.as_view()),
-    url(r'^verify-email$', VerifyEmailView.as_view()),
-    url(r'^password-reset$', PasswordResetView.as_view()),
-    url(r'^profile$', ProfileView.as_view()),
+    url(r'^login/$', LogInView.as_view()),
+    url(r'^register/$', RegisterView.as_view()),
+    url(r'^verify-email/$', VerifyEmailView.as_view()),
+    url(r'^password-reset/$', PasswordResetView.as_view()),
+    url(r'^profile/$', ProfileView.as_view()),
 ]
