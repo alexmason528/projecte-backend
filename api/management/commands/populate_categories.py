9817df72a93ad236645b8088cdefc28bb1dc2432
Alex Mason <alexmason528@hotmail.com>
2019-01-25 07:12:10 -0500

    Updated populate categories command

--- api/management/commands/populate_categories.py
+++ api/management/commands/populate_categories.py
@@ -1,3 +1,4 @@
+import json
 import csv
 
 from django.core.management.base import BaseCommand
@@ -5,7 +6,7 @@ from api.models import Category
 
 
 class Command(BaseCommand):
-    help = 'Import branding theme IDs from Xero'
+    help = 'Populate categories'
 
     def handle(self, *args, **options):
         with open('csv/categories.csv') as csvfile:
@@ -13,11 +14,12 @@ class Command(BaseCommand):
             for row in reader:
                 try:
                     Category.objects.create(
-                        id=row.get('Id').strip(),
-                        name=row.get('Name').strip(),
-                        slug=row.get('Slug').strip(),
-                        path=row.get('Path').strip(),
-                        parent_id=row.get('ParentID_Fk').strip()
+                        id=row.get('id').strip(),
+                        name=row.get('name').strip(),
+                        slug=row.get('slug').strip(),
+                        path=row.get('path').strip(),
+                        parent_id=row.get('parent_id').strip(),
+                        translation=json.loads(row.get('translation').strip()),
                     )
                 except Exception as e:
                     print(str(e))
